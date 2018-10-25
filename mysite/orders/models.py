7fc8c202387d131260bb76d4a1672ee4063759e5
liquanming <864799077@qq.com>
2018-10-25 09:14:28 +0800

    添加订单模型和视图

--- mysite/orders/models.py
+++ mysite/orders/models.py
@@ -1,5 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Order(models.Model):
-	id = models
+	id = models.AutoField(primary_key=True)
+	# 订单所属用户
+	user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="所属用户")
+	# 订单生成时间
+	create_time = models.DateTimeField(auto_now_add=True, verbose_name="订单生成时间")
+	# 收件人
+	recv_name = models.CharField(max_length=100, verbose_name="收件人")
+	# 收件人联系方式
+	recv_tel = models.CharField(max_length=50, verbose_name="收件人电话")
+	# 售货地址
+	recv_address = models.CharField(max_length=255, verbose_name="收货地址")
+	# 订单总金额
+	all_price = models.FloatField(verbose_name="订单总金额")
+	# 订单备注
+	remake = models.CharField(max_length=255, verbose_name="备注信息")
+
+
+class OrderItem(models.Model):
+	id = models.AutoField(primary_key=True)
+	# 商品编号
+	goods_id = models.IntegerField(verbose_name="商品编号")
+	# 商品图片
+	goods_img = models.CharField(max_length=255, verbose_name="商品图片")
+	# 商品名称
+	goods_name = models.CharField(max_length=255, verbose_name="商品名称")
+	# 商品价格
+	goods_price = models.FloatField(verbose_name="商品价格")
+	# 购买数量
+	goods_count = models.IntegerField(verbose_name="商品数量")
+	# 商品总价
+	goods_price_all = models.FloatField(verbose_name="成交价格")
+	# 所属订单
+	order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name="所属订单")
--- mysite/orders/views.py
+++ mysite/orders/views.py
@@ -1,26 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views.decorators.http import require_POST
 
 from shopcar.models import ShopCart
 from goods.models import Goods
+from . import models
+# from user.models import Address
 
 @require_POST
 def confirm(request):
 	g_ids = request.POST.getlist("g_id")
-	goods = ShopCart.goods.objects.filter(pk__in=g_ids)
-	pass
+	shop_carts = ShopCart.objects.filter(pk__in=g_ids)
+	# addresses = Address.objects.filter(user=request.user)
+	return render(request, "oeders/confirm.html", {"shop_carts":shop_carts})#, "addresses":addresses})
 
 
 def pay(reuqest):
 	pass
 
 
+@require_POST
 def done(request):
-	pass
+	c_ids = request.POST.getlist("c_id")
+	address_id = request.POST["address"]
+
+	# address = Address.objects.get(pk=address_id)
+	shopcarts = ShopCart.objects.filter(pk__in=c_ids)
+
+	_address = address.recv_name + "|" + address.recv_tel + "|" + address.province + "|" + \
+			   address.city + "|" + address.area + "|" + address.street + "|" + address.desc
+
+	# 生成订单
+	order = models.Order(address=address, user=request.user, recv_name=address.recv_name, recv_tel=address.recv_tel, \
+						  all_price=0, remake="")
+
+	order.save()
+	allCount = 0
+	for s in shopcarts:
+		g = s.goods
+		orderItem = models.OrderItem(goods_id=g.id, goods_img=g.goodsimage_set.all.firet.path,\
+						goods_name=g.anme, goods_price=g.price, goods_count=s.count, \
+						 goods_price_all=s.allTital)
+
+		orderItem.save()
+		allCount += s.allTotal
+	order.all_price = allCount
+	order.save()
+
+	return redirect(reverse("orders:list"))
 
 
 def list(request):
-	pass
+	orders = models.Order.objects.filter(user=request.user)
+	return render(request, "orders/list.html", {"orders":orders})
 
 
 def detail(request, o_id):
